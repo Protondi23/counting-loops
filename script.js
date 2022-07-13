6baf67d7be4ed62894e0b5c41e862f51108fb381
Protondi23@gmail.com <Protondi23@gmail.com>
2022-07-13 18:26:50 -0400

    commit

---
+++ script.js
@@ -0,0 +1,35 @@
+for (let i = 1; i <= 10 ; i++) {
+  console.log(i);
+}
+
+for(let i = 10; i >= 1; i--){
+   console.log(i);
+}
+
+let i = 0;
+    while (i < 10){
+        i++;
+        console.log(i);
+    }
+
+let x = 11;
+    while (x > 1){
+        x--;
+        console.log(x);
+    }
+{
+let a = 0;
+    do {
+        a++;
+        console.log(a);
+    }
+    while (a < 10);
+}
+{
+let b = 11;
+    do{
+        b--;
+        console.log(b);
+    }
+    while (b > 1);
+}
